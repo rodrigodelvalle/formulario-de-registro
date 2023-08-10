1db073aa26c819d3ce08d2885719b5b879a7c462
niconilson <niconilson@hotmail.com>
2023-08-10 19:15:20 -0300

    Largo mínimo de contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,12 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function verificarPassword() {
+    var inputName = document.querySelector("#password1").value;
+    if (inputName.length >= 6) {
+         showAlertSuccess(); // Puedes colocar un alert() o nada.
+}
+else {
+     showAlertError();
+}}
