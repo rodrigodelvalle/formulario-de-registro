2f950ec1bdd1eac69c8e4d441bf072265238c8af
rodrigodelvalle <rodrigodelvalle22@hotmail.com>
2023-08-11 01:11:56 -0300

    Actualización en validación

--- js/validacion.js
+++ js/validacion.js
@@ -6,32 +6,17 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-
 function verificarPassword() {
 
 let nombre = document.getElementById("nombre").value;
 let apellido = document.getElementById("apellido").value;
 let email = document.getElementById("email").value;
-let checkbox = document.getElementById("terminos").checked;
+let chbx = document.getElementById("terminos").checked;
 
     var inputName = document.querySelector("#password1").value;
-    if (inputName.length >= 6 && (document.querySelector("#password1").value == document.querySelector("#password2").value) && (nombre !== "") && (apellido !== "") && (email !== "") && checkbox) {
+    if (inputName.length >= 6 && (document.querySelector("#password1").value == document.querySelector("#password2").value) && nombre !== "" && apellido !== "" && email !== "" && chbx) {
          showAlertSuccess(); 
 }
 else {
      showAlertError();
 }}
-
-// nueva rama NicoN
-
-let checkbox = document.getElementById ("terminos");
-
-checkbox.addEventListener('click', function(){
-    if (checkbox.checked) {
-        showAlertSuccess ()
-    }
-    else {
-        showAlertError ()
-    }
-})
-
